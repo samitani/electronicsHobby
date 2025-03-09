dd830acf34be8a3f79b02d9c1e48e79d03e69d23
Satoshi Mitani <mitans02@yahoo.co.jp>
2025-03-09 19:52:47 +0900

    initial commit

---
+++ m5stamp-c3u/CraneGame/README.md
@@ -0,0 +1,16 @@
+# What is this ?
+
+CraneGame but it's only has 2 axies.
+
+# Parts
+
+| Parts            | Name                 |
+|------------------|----------------------|
+| Micro Controller | M5Stamp C3U      x 1 |
+| Motor Driver     | Toshiba TB6674PG x 2 |
+| Stepper Motor    | MDP-35A          x 1 |
+| Stepper Motor    | [SM-42BYG011](https://akizukidenshi.com/catalog/g/g105372/)      x 1 |
+| Joystick Module  | [ジョイスティックRKJXV122400R DIP化キット](https://akizukidenshi.com/catalog/g/g115433/) |
+
+# Breadboard
+TBD
---
+++ m5stamp-c3u/CraneGame/src/cranegame.ino
@@ -0,0 +1,124 @@
+#include <math.h>
+#include <Adafruit_NeoPixel.h>
+#include <ESP32Servo.h>
+
+#define LED_PIN 2
+#define BTN_PIN 10
+#define SERVO_PIN 8
+
+#define TB6647PG_1_S_PIN 3
+#define TB6647PG_2_S_PIN 4
+#define TB6647PG_A_PIN 5
+#define TB6647PG_B_PIN 6
+#define MOTOR_DELAY_MS 3
+
+Adafruit_NeoPixel pixels(1, LED_PIN, NEO_GRB + NEO_KHZ800);
+Servo motor_servo;
+int valueX, valueY, valueBtn, isArmOpen;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BTN_PIN, INPUT_PULLUP);
+
+  pinMode(TB6647PG_1_S_PIN,OUTPUT);
+  pinMode(TB6647PG_2_S_PIN,OUTPUT);
+  pinMode(TB6647PG_A_PIN,OUTPUT);
+  pinMode(TB6647PG_B_PIN,OUTPUT);
+
+  pixels.begin();
+  motor_servo.attach(SERVO_PIN);
+
+  isArmOpen = 0;
+}
+
+void motor_backword() {
+    digitalWrite(TB6647PG_A_PIN, HIGH);
+    digitalWrite(TB6647PG_B_PIN, HIGH);
+    delay(MOTOR_DELAY_MS);
+
+    digitalWrite(TB6647PG_A_PIN, HIGH);
+    digitalWrite(TB6647PG_B_PIN, LOW);
+    delay(MOTOR_DELAY_MS);
+
+    digitalWrite(TB6647PG_A_PIN, LOW);
+    digitalWrite(TB6647PG_B_PIN, LOW);
+    delay(MOTOR_DELAY_MS);
+
+    digitalWrite(TB6647PG_A_PIN, LOW);
+    digitalWrite(TB6647PG_B_PIN, HIGH);
+    delay(MOTOR_DELAY_MS);
+}
+
+void motor_forword() {
+    digitalWrite(TB6647PG_A_PIN, HIGH);
+    digitalWrite(TB6647PG_B_PIN, HIGH);
+    delay(MOTOR_DELAY_MS);
+
+    digitalWrite(TB6647PG_A_PIN, LOW);
+    digitalWrite(TB6647PG_B_PIN, HIGH);
+    delay(MOTOR_DELAY_MS);
+
+    digitalWrite(TB6647PG_A_PIN, LOW);
+    digitalWrite(TB6647PG_B_PIN, LOW);
+    delay(MOTOR_DELAY_MS);
+
+    digitalWrite(TB6647PG_A_PIN, HIGH);
+    digitalWrite(TB6647PG_B_PIN, LOW);
+    delay(MOTOR_DELAY_MS);
+}
+
+void loop() {
+  valueX = analogRead(0);
+  valueY = analogRead(1);
+  valueBtn = digitalRead(BTN_PIN);
+ 
+  if (valueX > 4050) { // UP
+    pixels.setPixelColor(0, pixels.Color(128, 0, 0));
+    pixels.show();
+
+    digitalWrite(TB6647PG_1_S_PIN, HIGH);
+    digitalWrite(TB6647PG_2_S_PIN, LOW);
+    motor_backword();
+  } else if (valueX < 300) { // DOWN
+    pixels.setPixelColor(0, pixels.Color(0, 0, 128));
+    pixels.show();
+
+    digitalWrite(TB6647PG_1_S_PIN, HIGH);
+    digitalWrite(TB6647PG_2_S_PIN, LOW);
+    motor_forword();
+  } else if (valueY > 4050) { // RIGHT
+    pixels.setPixelColor(0, pixels.Color(0, 128, 0));
+    pixels.show();
+
+    digitalWrite(TB6647PG_1_S_PIN, LOW);
+    digitalWrite(TB6647PG_2_S_PIN, HIGH);
+    motor_forword();
+  } else if (valueY < 700) { // LEFT
+    pixels.setPixelColor(0, pixels.Color(128, 128, 128));
+    pixels.show();
+
+    digitalWrite(TB6647PG_1_S_PIN, LOW);
+    digitalWrite(TB6647PG_2_S_PIN, HIGH);
+    motor_backword();
+  } else if (!valueBtn) {
+    pixels.setPixelColor(0, pixels.Color(128, 128, 0));
+    pixels.show();
+
+    if (isArmOpen) {
+      motor_servo.write(0);
+    } else {
+      motor_servo.write(180);
+    }
+    isArmOpen = !isArmOpen;
+    delay(1000);
+  } else {
+    digitalWrite(TB6647PG_1_S_PIN, LOW);
+    digitalWrite(TB6647PG_2_S_PIN, LOW);
+
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+    delay(300);
+  }
+
+
+}
